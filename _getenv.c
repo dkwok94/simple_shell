4be68c5037f36260cbc1087d2acea07d418cb54f
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-03-17 23:30:00 +0000

    Add functionality to print all directories in PATH var on separate lines

--- _getenv.c
+++ _getenv.c
@@ -22,6 +22,56 @@ char *_getenv(const char *name)
 	return (NULL);
 }
 /**
+ *_strlen - calculates the length of a string
+ *@buf: buffer to count length of strings
+ *
+ *Return: the length of the string
+ */
+int _strlen(char *buf)
+{
+	int i = 0;
+
+	while (buf[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ *printpath - prints each entry of path
+ *@path: pointer to the path variable
+ *
+ *Return: 0 upon success
+ */
+int printpath(char *path)
+{
+	char buf[1024];
+	int i = 0, j = 0, length;
+	while (path[i] != '\0')
+	{
+		if (i == 0)
+		{
+			while (path[i] != '=')
+				i++;
+			i++;
+		}
+		while (path[i] != ':' && path[i] != '\0')
+		{
+			buf[j] = path[i];
+			i++;
+			j++;
+		}
+		if (path[i] == ':')
+			i++;
+		buf[j++] = '\n';
+		buf[j] = '\0';
+		length = _strlen(buf);
+		write(1, buf, length);
+		j = 0;
+	}
+	return (0);
+}
+/**
  *main - entry point
  *
  *Description: Tests the code
@@ -39,6 +89,14 @@ int main(void)
 	}
 	printf("Environmental variable: %s\n", envvar);
 
+	envvar = _getenv("LS_COLORS");
+	if (envvar == NULL)
+	{
+		printf("Could not find environmental variable\n");
+		return (1);
+	}
+	printf("Environmental variable: %s\n", envvar);
+
 	envvar = _getenv("PATH");
 	if (envvar == NULL)
 	{
@@ -47,12 +105,6 @@ int main(void)
 	}
 	printf("Environmental variable: %s\n", envvar);
 
-	envvar = _getenv("LS_COLORS");
-	if (envvar == NULL)
-	{
-		printf("Could not find environmental variable\n");
-		return (1);
-	}
-	printf("Environmental variable: %s\n", envvar);
+	printpath(envvar);
 	return (0);
 }
